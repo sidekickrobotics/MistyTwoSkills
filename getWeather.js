90d01e9acfa2074ca0003a81a0f4e50b88d5eb8f
sidekickrobotics <50274375+sidekickrobotics@users.noreply.github.com>
2019-10-16 12:28:38 -0400

    Add files via upload

---
+++ getWeather.js
@@ -0,0 +1,22 @@
+
+// Sends a message to debug listeners
+misty.Debug("The Weather skill is starting! - External Request")
+
+
+misty.SendExternalRequest(
+    "GET",
+    "http://api.weatherstack.com/current?access_key="+_params.access_key+"&query="+_params.query
+    );
+
+function _SendExternalRequest(data) {
+
+    _data = JSON.parse(data.Result.ResponseObject.Data)
+
+    _temperature = _data.current.temperature
+    _weather_descriptions = _data.current.weather_descriptions
+
+    _name = _data.location.name
+
+    misty.Debug("Misty here! Just letting you know it's " + _temperature + " and " + _weather_descriptions + " in " + _name);
+
+}
