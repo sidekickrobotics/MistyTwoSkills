0260e242eccbd3261913e9952a13f6ec0f756351
sidekickrobotics <50274375+sidekickrobotics@users.noreply.github.com>
2019-11-26 11:08:44 -0500

    Update header with date written and license

--- whoForHalloween/whoForHalloween.js
+++ whoForHalloween/whoForHalloween.js
@@ -1,3 +1,29 @@
+/**********************************************************************
+My "Who for Halloween?" Misty skill"
+
+\Misty II plays different characters for Halloween
+
+Date First Written: 10/17/2019
+Date Last Update: 11/26/2019
+Date Last Tested: 11/26/2019
+
+This is experimental CODE, used for my learning.
+*********************************************************************************/
+
+/* Copyright 2019 Aaron Rues
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
+
 // Sends a message for debug 
 misty.Debug("The Who for Halloween? Skill is starting!")
 
