5eb9678837e3e4545aa08cacfefbe5748cd5b7e1
sidekickrobotics <50274375+sidekickrobotics@users.noreply.github.com>
2019-11-26 11:29:27 -0500

    In showoff, change LED back to purple at end

--- getHousePowerUse/getHousePowerUse.js
+++ getHousePowerUse/getHousePowerUse.js
@@ -1,26 +1,28 @@
-/*
-Apache 2.0 License
+/**********************************************************************
+getHousePowerUse - Monitors a wifi connected whole house power meter
 
-Copyright (c) 2019 sidekickrobotics
+Testing on Misty II  
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+Date First Written: 10/23/2019
+Date Last Update: 11/26/2019
+Date Last Tested: 11/26/2019
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+This is experimental CODE, used for my learning.
+*********************************************************************************/
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
+/* Copyright 2019 Aaron Rues
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
 
 
 // Description: Misty Skill reads power consumption from a connected home power
@@ -133,6 +135,8 @@ if (showOffSkill){
     misty.Pause(3000);
     
     misty.DisplayImage("e_DefaultContent.jpg"); //Set back to default 
+    misty.ChangeLED(100, 70, 160); //Default LED
+    misty.Pause(2000);
     misty.SetBlinking(true);
     misty.Debug("Ending Showing-Off now");
     
